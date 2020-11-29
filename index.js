d50ffa06b6922aa67e2382240a2a34db2e1c460b
Matthew R. Becker <becker.mr@gmail.com>
2020-11-29 06:48:34 -0600

    DEBUG

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const exec = require('@actions/exec').exec
 const path = require('path')
 
 async function execute (command) {
-  console.log('bash -c "' + command + '"')
-  await exec(command)
+  try {
+    console.log('bash -c "' + command + '"')
+    await exec('bash -c "' + command + '"')
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
 async function run () {
@@ -15,25 +19,22 @@ async function run () {
     console.log(envFileName)
     const envFilePath = path.join(process.env.GITHUB_WORKSPACE || '', envFileName)
     console.log(envFilePath)
-
     const envYaml = yaml.safeLoad(fs.readFileSync(envFilePath, 'utf-8'))
     const envName = envYaml.name
-    core.startGroup('Installing environment ' + envName + '...')
 
-    execute('rm -f ~/.bashrc')
-    execute('rm -f ~/.bash_profile')
-    execute('rm -f ~/.profile')
-    execute('touch ~/.bashrc')
-    execute('wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba --strip-components=1')
-    execute('./micromamba shell init -s bash -p ~/micromamba')
-    execute('mkdir -p ~/micromamba/pkgs/')
-    execute('echo "set -eo pipefail" >> ~/.bashrc')
-    execute('echo "micromamba activate ' + envName + '" >> ~/.bashrc')
-    execute('mv ~/.bashrc ~/.profile')
-    execute('source ~/.profile && micromamba create -f ' + envFilePath + ' -y')
+    console.log('Installing environment ' + envName + '...')
 
-    execute('')
-    core.endGroup()
+    await execute('rm -f ~/.bashrc')
+    await execute('rm -f ~/.bash_profile')
+    await execute('rm -f ~/.profile')
+    await execute('touch ~/.bashrc')
+    await execute('wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba --strip-components=1')
+    await execute('./micromamba shell init -s bash -p ~/micromamba')
+    await execute('mkdir -p ~/micromamba/pkgs/')
+    await execute('echo "set -eo pipefail" >> ~/.bashrc')
+    await execute('echo "micromamba activate ' + envName + '" >> ~/.bashrc')
+    await execute('mv ~/.bashrc ~/.profile')
+    await execute('source ~/.profile && micromamba create -f ' + envFilePath + ' -y')
   } catch (error) {
     core.setFailed(error.message)
   }
