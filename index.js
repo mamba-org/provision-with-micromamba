925a60c1ce20dd8858cf2a17174ad121b7eccbce
Matthew R. Becker <becker.mr@gmail.com>
2020-11-29 07:49:11 -0600

    touch in js

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ async function execute (command) {
   }
 }
 
+function touch (filename) {
+  // https://remarkablemark.org/blog/2017/12/17/touch-file-nodejs/
+  const time = new Date()
+
+  try {
+    fs.utimesSync(filename, time, time)
+  } catch (err) {
+    fs.closeSync(fs.openSync(filename, 'w'))
+  }
+}
+
 async function run () {
   try {
     const envFileName = core.getInput('environment-file')
@@ -28,14 +39,14 @@ async function run () {
     await io.rmRF(path.join(os.homedir(), '.bashrc'))
     await io.rmRF(path.join(os.homedir(), '.bash_profile'))
     await io.rmRF(path.join(os.homedir(), '.profile'))
-    await execute('touch ~/.bashrc')
+    touch(path.join(os.homedir(), '.bashrc'))
     await execute('wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba --strip-components=1')
     await execute('./micromamba shell init -s bash -p ~/micromamba')
     await io.mkdirP(path.join(os.homedir(), 'micromamba/pkgs/'))
     await io.mv(path.join(os.homedir(), '.bashrc'), path.join(os.homedir(), '.profile'))
     await execute('source ~/.profile && micromamba create -f ' + envFilePath + ' -y')
-    fs.appendFileSync(path.join(os.homedir(), '.bashrc'), 'set -eo pipefail\n')
-    fs.appendFileSync(path.join(os.homedir(), '.bashrc'), 'micromamba activate\n' + envName)
+    fs.appendFileSync(path.join(os.homedir(), '.profile'), 'set -eo pipefail\n')
+    fs.appendFileSync(path.join(os.homedir(), '.profile'), 'micromamba activate\n' + envName)
   } catch (error) {
     core.setFailed(error.message)
   }
