6c7c7b22a5c545f0a814618c7b37813d756989c7
Matthew R. Becker <becker.mr@gmail.com>
2020-11-29 09:52:17 -0600

    activate base?

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function run () {
     await execute('wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba --strip-components=1')
     await execute('./micromamba shell init -s bash -p ~/micromamba')
     await io.mkdirP(path.join(os.homedir(), 'micromamba/pkgs/'))
-    await execute('source ~/.bashrc && micromamba activate base && micromamba install mamba -y')
+    await execute('source ~/.bashrc && micromamba activate base && micromamba install -c conda-forge mamba -y')
     await execute('source ~/.bashrc && micromamba activate base && mamba create -f ' + envFilePath + ' -y')
     fs.appendFileSync(bashrc, 'set -eo pipefail\n')
     fs.appendFileSync(bashrc, 'micromamba activate ' + envName + '\n')
