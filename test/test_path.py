5aec97fc16b7251e04a31f2947a9b8e0d25c3c64
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-10-22 15:16:55 +0200

    ..

---
+++ test/test_path.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+if __name__ == '__main__':
+	if os.name == 'nt':
+		assert(os.path.exists(os.path.expanduser("~/micromamba-bin/micromamba.exe")))
+	else:
+		assert(os.path.exists(os.path.expanduser("~/micromamba-bin/micromamba")))
+	assert(os.path.expanduser("~/micromamba-bin") in sys.path)
+	assert(shutil.which('micromamba') is not None)
+	exit(0)
