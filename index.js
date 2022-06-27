23fc0a3d23bce1e5b6e554e915161f48c55e6048
Jonas Haag <jonas@lophus.org>
2022-06-27 14:59:50 +0200

    Fix mac

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ const PATHS = {
 
 function getCondaArch () {
   const arch = {
-    [['osx', 'arm64']]: 'osx-arm64',
-    [['osx', 'x64']]: 'osx-64',
+    [['darwin', 'arm64']]: 'osx-arm64',
+    [['darwin', 'x64']]: 'osx-64',
     [['linux', 'x64']]: 'linux-64',
     [['linux', 'arm64']]: 'linux-aarch64',
     [['win32', 'x64']]: 'win-64'
