f7e2daeac39b2eebc1940ad6b7e0d59a290c9113
Jonas Haag <jonas@lophus.org>
2022-10-25 16:32:12 +0200

    WIP

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ function makeFinalCondaRcOptions (inputs, envYaml) {
     condaRcOptions.channel_priority = inputs.deprecatedChannelPriority
   }
   if (inputs.deprecatedChannels && !condaRcOptions.channels) {
-    condaRcOptions.channels = inputs.deprecatedChannelPriority
+    condaRcOptions.channels = inputs.deprecatedChannels
   }
   if (!condaRcOptions.channels?.length && !envYaml && !yamlFileHasKey(PATHS.condaRc)) {
     condaRcOptions.channels = DEFAULT_CHANNELS
